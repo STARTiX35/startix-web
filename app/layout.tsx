1b392ce741eba3007e0acbf19cd195d24ea2a613
細井崚吾 <s2410615@u.tsukuba.ac.jp>
2025-04-14 15:50:25 +0900

    Update something

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,9 @@ import './globals.css';
 export const metadata: Metadata = {
   title: 'STARTiX - 筑波大学起業サークル',
   description: 'STARTiX - 筑波大学の起業サークル。未来の起業家を、今ここから。',
+  verification: {
+    google: 'Agz0QsTpbFd7g_rTVzNb_-1d1zaq40uWSAeaPgVTe6g',
+  },
 };
 
 export default function RootLayout({
