9b7dc88067afc79c77af554b9fbb3cb9b23c02b2
細井崚吾 <s2410615@u.tsukuba.ac.jp>
2025-04-19 22:33:34 +0900

    環境変数名変更

--- next.config.js
+++ next.config.js
@@ -5,10 +5,6 @@ const nextConfig = {
   images: {
     domains: ['images.microcms-assets.io'],
   },
-  env: {
-    MICROCMS_SERVICE_DOMAIN: process.env.MICROCMS_SERVICE_DOMAIN,
-    MICROCMS_API_KEY: process.env.MICROCMS_API_KEY,
-  },
   // 必要に応じて設定を追加
 };
 
