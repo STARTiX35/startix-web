80b4d967477fd1afe7fa08fc0cfc6665f342cb07
kou7306 <kouta.1206.y@icloud.com>
2025-06-26 21:28:59 +0900

    ヘッダーとフッターの修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,12 @@ import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export const metadata: Metadata = {
-  title: "STARTiX - 筑波大学起業サークル",
-  description: "STARTiX - 筑波大学の起業サークル。未来の起業家を、今ここから。",
+  title: "STARTiX - 筑波大学発インカレ起業サークル",
+  description:
+    "STARTiXは、筑波大学を中心に活動するインカレの学生起業家サークルです。起業に興味がある仲間と共に、アイデアを形にし、社会に新しい価値を提供することを目指しています。",
   verification: {
     google: "Agz0QsTpbFd7g_rTVzNb_-1d1zaq40uWSAeaPgVTe6g",
   },
@@ -22,7 +24,8 @@ export default function RootLayout({
     <html lang="ja">
       <body>
         <Header />
-        {children}
+        <main>{children}</main>
+        <Footer />
         <Analytics />
       </body>
     </html>
