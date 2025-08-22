958773fa8f1d7ef4a47ad4f72da42bcaf89bd608
ryogohosoi35 <hosoiryogo@gmail.com>
2025-08-22 09:51:25 +0900

    SEO対策を実施

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ import Footer from "./components/Footer";
 export const metadata: Metadata = {
   title: "STARTiX - 筑波大学発インカレ起業サークル",
   description:
-    "STARTiXは、筑波大学を中心に活動するインカレの学生起業家サークルです。起業に興味がある仲間と共に、アイデアを形にし、社会に新しい価値を提供することを目指しています。",
+    "STARTiXは、筑波大学の学生が中心となって活動する起業サークルです。「本気で夢を語り合える場所を作る」をミッションに、起業を志す大学生が集まり、アイデアを形にしています。",
   verification: {
     google: "Agz0QsTpbFd7g_rTVzNb_-1d1zaq40uWSAeaPgVTe6g",
   },
